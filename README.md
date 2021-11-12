0d280451e51770e1be3f409a9c242fb03b64af93
xAliTura01 <cetinalitugra@gmail.com>
2021-11-12 20:33:15 +0300

    Hello World

---
+++ README.md
@@ -0,0 +1,36 @@
+<p align="center">
+    <img src="https://github.com/xAliTura01/Ratio/blob/master/icon.png" width="144" height="144" alt="Ratio">
+</p>
+
+<h1 align="center">Ratio</h1>
+<p align="center">Ratio plugin is a luck plugin. The more lucky you are, the more you win.</p>
+
+<br>
+<p align="center">
+    <a href="https://poggit.pmmp.io/p/Ratio">
+        <img src="https://poggit.pmmp.io/shield.state/Ratio" alt="Plugin version">
+    </a>
+    <a href="https://github.com/pmmp/PocketMine-MP">
+        <img src="https://poggit.pmmp.io/shield.api/Ratio" alt="API version">
+    </a>
+    <a href="https://poggit.pmmp.io/p/Ratio">
+        <img src="https://poggit.pmmp.io/shield.dl/Ratio" alt="Downloads on Poggit">
+    </a>
+    <a href="https://github.com/xAliTura01/Ratio/blob/master/LICENSE">
+        <img src="https://img.shields.io/badge/License-GPLv3-blue.svg" alt="License">
+      </p>    
+
+## Features
+- When you break a block (Cobblestone), it gives/puts you something random.
+
+## Support
+Since it's new, it's missing a lot, if you want to help, don't hesitate!
+
+You can:
+- [open an issue](https://github.com/xAliTura01/Ratio/issues) (bug fix and feature requests etc.)
+- [create a pull request](https://github.com/xAliTura01/Ratio/pulls) (code contributions like fixed bugs or added features)
+
+##### *NOTE:* In the ratio plugin when you break the cobblestone, something empty may come out by chance, don't worry, this is a luck plugin, and if you see something missing, please don't forget to report it.
+
+## Credits
+Thanks for the icon from www.flaticon.com
---
+++ src/xAliTura01/Ratio/Main.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace xAliTura01\Ratio;
+
+use pocketmine\Player;
+use pocketmine\item\Item;
+use pocketmine\block\Block;
+use pocketmine\math\Vector3;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\block\BlockFactory;
+
+use ReflectionClass;
+use pocketmine\resourcepacks\ZippedResourcePack;
+use pocketmine\network\mcpe\protocol\PlaySoundPacket;
+
+class Main extends PluginBase implements Listener
+{
+
+	public function onEnable() : void
+	{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+		$this->saveResource("Ratio.mcpack", true);
+
+		$manager = $this->getServer()->getResourcePackManager();
+		$pack = new ZippedResourcePack($this->getDataFolder() . "Ratio.mcpack");
+
+		$reflection = new ReflectionClass($manager);
+
+		$property = $reflection->getProperty("resourcePacks");
+		$property->setAccessible(true);
+
+		$currentResourcePacks = $property->getValue($manager);
+		$currentResourcePacks[] = $pack;
+		$property->setValue($manager, $currentResourcePacks);
+
+		$property = $reflection->getProperty("uuidList");
+		$property->setAccessible(true);
+		$currentUUIDPacks = $property->getValue($manager);
+		$currentUUIDPacks[strtolower($pack->getPackId())] = $pack;
+		$property->setValue($manager, $currentUUIDPacks);
+
+		$property = $reflection->getProperty("serverForceResources");
+		$property->setAccessible(true);
+		$property->setValue($manager, true);
+	}
+
+	public function blockBreak(BlockBreakEvent $event)
+	{
+		$player = $event->getPlayer();
+		$name = $player->getName();
+		$world = $player->getLevel()->getFolderName();
+		if ($world = $name) {
+			if ($event->getBlock()->getId() == 4) {
+				if ($player->hasPermission("ratio.vip")) {
+					$this->ratioVip($event);
+				}else{
+					$this->ratioPlayer($event);
+				}
+			}
+		}
+	}
+
+	public function playMusic(Player $player)
+	{
+		$packet = new PlaySoundPacket();
+		$packet->soundName = "Ratio";
+		$packet->x = $player->getX();
+		$packet->y = $player->getY();
+		$packet->z = $player->getZ();
+		$packet->volume = 1;
+		$packet->pitch = 1;
+		$player->sendDataPacket($packet);
+	}
+
+	public function ratioVip($event)
+	{
+		$player = $event->getPlayer();
+		$ratio1 = rand(1,30);
+		$ratio2 = rand(1,30);
+		$ratio3 = rand(1,35);
+		$w = $event->getPlayer()->getLevel()->getFolderName();
+		$world = $this->getServer()->getLevelByName($w);
+		$x = $event->getBlock()->getX();
+		$y = $event->getBlock()->getY();
+		$z = $event->getBlock()->getZ();
+		switch ($ratio1) {
+			case 1:
+				$world->setBlock(new Vector3($x,$y,$z), Block::get(16));
+				$this->playMusic($player);
+			break;
+			case 2:
+				$world->dropItem(new Vector3($x,$y,$z), Item::get(266));
+				$this->playMusic($player);
+			break;
+			case 3:
+				$world->setBlock(new Vector3($x,$y,$z), Block::get(14));
+				$this->playMusic($player);
+			break;
+			case 4:
+				$world->dropItem(new Vector3($x,$y,$z), Item::get(370));
+				$this->playMusic($player);
+			break;
+		}
+		switch ($ratio2) {
+			case 1:
+				$world->setBlock(new Vector3($x,$y,$z), Block::get(15));
+				$this->playMusic($player);
+			break;
+			case 2:
+				$world->dropItem(new Vector3($x,$y,$z), Item::get(266));
+				$this->playMusic($player);
+			break;
+			case 3:
+				$world->setBlock(new Vector3($x,$y,$z), Block::get(129));
+				$this->playMusic($player);
+			break;
+			case 4:
+				$world->dropItem(new Vector3($x,$y,$z), Item::get(265));
+				$this->playMusic($player);
+			break;
+		}
+		switch($ratio3){
+			case 1:
+				$world->setBlock(new Vector3($x,$y,$z), Block::get(56));
+				$this->playMusic($player);
+			break;
+			case 2:
+				$world->dropItem(new Vector3($x,$y,$z), Item::get(264));
+				$this->playMusic($player);
+			break;
+		}
+	}
+
+	public function ratioPlayer($event)
+	{
+		$player = $event->getPlayer();
+		$ratio1 = rand(1,90);
+		$ratio2 = rand(1,90);
+		$ratio3 = rand(1,90);
+		$w = $event->getPlayer()->getLevel()->getFolderName();
+		$world = $this->getServer()->getLevelByName($w);
+		$x = $event->getBlock()->getX();
+		$y = $event->getBlock()->getY();
+		$z = $event->getBlock()->getZ();
+		switch ($ratio1) {
+			case 1:
+				$world->setBlock(new Vector3($x,$y,$z), Block::get(370));
+				$this->playMusic($player);
+			break;
+			case 2:
+				$world->dropItem(new Vector3($x,$y,$z), Item::get(265));
+				$this->playMusic($player);
+			break;
+			case 3:
+				$world->setBlock(new Vector3($x,$y,$z), Block::get(266));
+				$this->playMusic($player);
+			break;
+			case 4:
+				$world->dropItem(new Vector3($x,$y,$z), Item::get(264));
+				$this->playMusic($player);
+			break;
+		}
+		switch ($ratio2) {
+			case 1:
+				$world->setBlock(new Vector3($x,$y,$z), Block::get(370));
+				$this->playMusic($player);
+			break;
+			case 2:
+				$world->dropItem(new Vector3($x,$y,$z), Item::get(265));
+				$this->playMusic($player);
+			break;
+			case 3:
+				$world->setBlock(new Vector3($x,$y,$z), Block::get(266));
+				$this->playMusic($player);
+			break;
+			case 4:
+				$world->dropItem(new Vector3($x,$y,$z), Item::get(266));
+				$this->playMusic($player);
+			break;
+		}
+		switch ($ratio3) {
+			case 1:
+				$world->setBlock(new Vector3($x,$y,$z), Block::get(4));
+				$this->playMusic($player);
+			break;
+			case 2:
+				$world->dropItem(new Vector3($x,$y,$z), Item::get(1));
+				$this->playMusic($player);
+			break;
+		}
+	}
+}
